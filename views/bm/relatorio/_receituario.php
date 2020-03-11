5296c152f8d4dc6f97a9e29b0eed8f6ab329414a
Rafael Bahia <41449291+bahia-rafael@users.noreply.github.com>
2020-03-11 10:35:25 -0300

    Arquivo de Receituario

---
+++ views/bm/relatorio/_receituario.php
@@ -0,0 +1,67 @@
+<style type="text/css">
+    span.cls_002 {
+        font-family: Arial, serif;
+        font-size: 7.7px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: italic;
+        text-decoration: none
+    }
+
+    div.cls_002 {
+        font-family: Arial, serif;
+        font-size: 7.7px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: italic;
+        text-decoration: none
+    }
+
+    span.cls_003 {
+        font-family: Arial, serif;
+        font-size: 7.7px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: italic;
+        text-decoration: none
+    }
+
+    div.cls_003 {
+        font-family: Arial, serif;
+        font-size: 7.7px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: italic;
+        text-decoration: none
+    }
+
+    span.cls_004 {
+        font-family: Arial, serif;
+        font-size: 8.6px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: normal;
+        text-decoration: none
+    }
+
+    div.cls_004 {
+        font-family: Arial, serif;
+        font-size: 8.6px;
+        color: rgb(127, 145, 154);
+        font-weight: normal;
+        font-style: normal;
+        text-decoration: none
+    }
+</style>
+
+<div style="position:absolute;left:50%;margin-left:-212px;top:0px;width:425px;height:595px;border-style:outset;overflow:hidden">
+    <div style="position:absolute;left:0px;top:0px">
+        <img src="resources/dist/img/background_receituario.jpg" width=425 height=595></div>
+    <div style="position:absolute;left:314.26px;top:153.55px" class="cls_002"><span class="cls_002">l</span><span class="cls_003">o</span></div>
+    <div style="position:absolute;left:69.63px;top:520.33px" class="cls_004"><span class="cls_004">Av. Sete de Setembro, 880,</span></div>
+    <div style="position:absolute;left:261.52px;top:520.04px" class="cls_004"><span class="cls_004">atendimento@vitalsorrir.com</span></div>
+    <div style="position:absolute;left:69.63px;top:531.07px" class="cls_004"><span class="cls_004">Ed. Poty, Sala 201-A, Dois de Julho,</span></div>
+    <div style="position:absolute;left:261.52px;top:530.78px" class="cls_004"><span class="cls_004">(71) 2137.3094</span></div>
+    <div style="position:absolute;left:69.63px;top:541.80px" class="cls_004"><span class="cls_004">CEP 40060-001, Salvador, Bahia</span></div>
+    <div style="position:absolute;left:261.52px;top:541.51px" class="cls_004"><span class="cls_004">(71) 99607.1727</span></div>
+</div>
